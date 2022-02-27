9937f0168bd3882f4e7256c13318ea176b0813ed
mityowaffler <mityowaffler@gmail.com>
2022-02-28 00:21:05 +0200

    initial commit

--- src/com/company/Homework1.java
+++ src/com/company/Homework1.java
@@ -1,7 +1,27 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Homework1 {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int number;
 
+        System.out.println("Enter a number");
+        number = scan.nextInt();
+        scan.close();
+
+        int absoluteValue;
+        if (number < 0) {
+            absoluteValue = (number * (-1));
+            System.out.println("The absolute value of the number: " +number + " is: " +absoluteValue);
+        } else {
+            System.out.println("The absolute value of the number: " + number + " is: " +number);
+        }
     }
 }
+
+
+
+
+
